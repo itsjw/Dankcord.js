240234aa0f48ef8e3a8bb36c5b2885d156275de6
Joseph <joseph@josephbanks.me>
2017-11-13 21:47:26 +0000

    Extend event emitter and load websocket correct this time

--- src/client.js
+++ src/client.js
@@ -1,11 +1,13 @@
 const {HTTPClient} = require("./routes.js");
 const {GatewayClient} = require("./gateway.js");
+const {EventEmitter} = require("events");
 
-class Client {
+class Client extends EventEmitter {
     constructor(token){
+        super();
         this.token = token;
         this.http = new HTTPClient(token);
-        this.gateway = new GatewayClient(token);
+        this.gateway = new GatewayClient(token, this);
     }
 }
 exports.Client = Client;
