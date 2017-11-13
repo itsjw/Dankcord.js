e383216b944c9b3aff49fad12d670ead5f766b8f
Joseph <joseph@josephbanks.me>
2017-11-13 19:03:24 +0000

    Export client

--- src/client.js
+++ src/client.js
@@ -1,8 +1,11 @@
-const {HTTPClient} = require("./routes.js")
+const {HTTPClient} = require("./routes.js");
+const {GatewayClient} = require("./gateway.js");
 
 class Client {
     constructor(token){
-        this.token = token
-        this.http = HTTPClient
+        this.token = token;
+        this.http = new HTTPClient(token);
+        this.gateway = new GatewayClient(token);
     }
 }
+exports.Client = Client;
