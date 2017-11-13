29f44fd0333005d9c03794337c4e9a0c521eb681
Joseph <joseph@josephbanks.me>
2017-11-13 19:54:16 +0000

    Add get gateway function

--- src/gateway.js
+++ src/gateway.js
@@ -2,5 +2,9 @@ class GatewayClient {
     constructor(token) {
         this.token = token
     }
+
+    getWebSocketURL(){
+        return "wss://gateway.discord.gg/?v=6&encoding=json"
+    }
 }
 exports.GatewayClient = GatewayClient;
