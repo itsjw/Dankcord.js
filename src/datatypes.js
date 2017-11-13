8aa8917936e545367c06e43f9bf1287ccfd33749
Joseph <joseph@josephbanks.me>
2017-11-13 22:36:30 +0000

    Add datatypes

--- src/datatypes.js
+++ src/datatypes.js
@@ -1,7 +1,11 @@
 class Message {
     constructor(data, client){
+        for(var i of Object.keys(data)){
+            this[i] = data[i];
+        }
         this.channel = client.channels.get(data["channel_id"]);
         this.content = data.content;
+
     }
 }
 exports.Message = Message;
