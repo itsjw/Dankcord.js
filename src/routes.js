b9ef0c3b2cf26218390ed4b58415aa7684648dad
Joseph <joseph@josephbanks.me>
2017-11-14 00:04:38 +0000

    Add required headers

--- src/routes.js
+++ src/routes.js
@@ -2,7 +2,9 @@ class HTTPClient {
     constructor(token){
         this.token = token;
         this.headers = {
-            "Authorization": `Bot ${this.token}`
+            "Authorization": `Bot ${this.token}`,
+            "User-Agent": "Dankcord.js (josephbanks.me, 1.0.0)",
+            "Content-Type": "application/json"
         };
     }
 }
