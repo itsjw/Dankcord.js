ea8fd8090e19372425af94a51b7964d757c6f4bf
Joseph <joseph@josephbanks.me>
2017-11-14 00:17:24 +0000

    route handler

--- src/routes.js
+++ src/routes.js
@@ -1,3 +1,5 @@
+const snekfetch = require("snekfetch");
+
 class HTTPClient {
     constructor(token){
         this.token = token;
@@ -6,6 +8,26 @@ class HTTPClient {
             "User-Agent": "Dankcord.js (josephbanks.me, 1.0.0)",
             "Content-Type": "application/json"
         };
+        this.url = "https://discordapp.com/";
+        this.base = "api/v6/";
+    }
+
+    sendReq(url, method, payload) {
+        url = this.url + this.base + url;
+        return new Promise((resolve) => {
+            switch(method){
+            case "get":
+                snekfetch.get(url, {headers: this.headers}).then((res) => {
+                    resolve(JSON.parse(res.text));
+                });
+                break;
+
+            case "post":
+                snekfetch.get(url, {headers: this.headers, data: payload}).then((res) => {
+                    resolve(JSON.parse(res.text));
+                });
+            }
+        });
     }
 }
 exports.HTTPClient = HTTPClient;
